65bd662cfaa3944b16c75e40dd10eb78650708fd
Bradley Brown <brownleroy41@ymail.com>
2022-03-20 17:57:11 -0400

    Initial Commit

---
+++ Reminder/ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderApp.swift
+//  Reminder
+//
+//  Created by Bradley Brown on 3/20/22.
+//
+
+import SwiftUI
+
+@main
+struct ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
